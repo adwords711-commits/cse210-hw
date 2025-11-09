ef63a44773ce88d632664d0b874f3c567e13c192
adwords711-commits <adwords711@gmail.com>
2025-11-09 01:46:57 -0500

    coompeted prep2 & prep4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random rnd = new Random();
+        int magicNumber = rnd.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+         
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it ");
+            }
+        }
+        
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finish. ");
+        List<int> numbers = new List<int>();
+        int newNumber = -1;
+
+        while (newNumber != 0)
+        {
+            Console.Write("Enter number ");
+            newNumber = int.Parse(Console.ReadLine());
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+
+            }
+        }
+        /* calculate the sum or total of the numbers in the list*/
+        int totalSum = 0;
+        foreach (int number in numbers)
+        {
+            totalSum += number;
+        }
+        Console.WriteLine($"The sum is: {totalSum} ");
+
+        /*Compute the average of the numbers in the list*/
+        double average = (double)totalSum / numbers.Count;
+        Console.WriteLine($"The average is: {average} ");
+
+        /*Compute the largest number*/
+        int largestNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {largestNumber} ");
+
+
     }
 }
