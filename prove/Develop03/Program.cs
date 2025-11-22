5460f235e40fac3fa63b48a3f8ce7c2cd6a1999c
adwords711-commits <adwords711@gmail.com>
2025-11-22 15:29:38 -0500

    program class completed

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Alma", 37, 6);
+        Scripture scripture = new Scripture(reference, "By small and simple things are great things brought to pass; and small means in many instances doth confound the wise");
+
+        while (!scripture.AllWordsHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetText());
+            Console.WriteLine("\nPress enter to continue or type 'quit' to finish: ");
+
+            string input = Console.ReadLine();
+            if (input == "quit")
+                break;
+            scripture.HideRandomWords();
+
+        }
     }
 }
